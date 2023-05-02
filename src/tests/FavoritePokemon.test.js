69e86556c7e2ee9773b7ebd149636821fd2de05d
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-02 18:44:25 -0300

    requisito 03 ok

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import pokemonList from '../data';
 import FavoritePokemon from '../pages/FavoritePokemon';
 
 describe(' Ao favoritar a partir da página de detalhes teste se:', () => {
   it('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
-    render(<FavoritePokemon />);
-    const found = screen.queryByText('No favorite Pokémon found');
-    expect(found).toBeInTheDocument();
+    const favoritePokemon = [];
+    renderWithRouter(<FavoritePokemon pokemonList={ favoritePokemon } />);
+
+    const NoFavoriteFound = screen.queryByText('No favorite Pokémon found');
+    expect(NoFavoriteFound).toBeInTheDocument();
+  });
+
+  it('Teste se apenas são exibidos os Pokémon favoritados.', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={ pokemonList } />);
+
+    pokemonList.forEach((pokemon) => {
+      const pokeName = screen.queryByText(pokemon.name);
+      expect(pokeName).toBeInTheDocument();
+    });
   });
 });
