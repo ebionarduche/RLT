c08da93403bc58a0aabd3682cf64c934855e43f8
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-02 17:39:12 -0300

    requisito 04 ok

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemon from '../pages/FavoritePokemon';
+
+describe(' Ao favoritar a partir da página de detalhes teste se:', () => {
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    render(<FavoritePokemon />);
+    const found = screen.queryByText('No favorite Pokémon found');
+    expect(found).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('testando se contém as informações sobre a Pokédex', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const NotFoundMessage = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(NotFoundMessage).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem', () => {
+    render(<NotFound />);
+    const img = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(img).toBeInTheDocument();
+  });
+});
