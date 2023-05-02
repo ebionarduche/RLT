77ade6d86d1efee21cde36a83f77ebdb8659b3f5
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-02 15:44:04 -0300

    requisito 01 ok

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testa se o topo aplicação contém links fixos de navegação', () => {
+  it('Os links devem possuir os textos específicos', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    const linkFavorite = screen.getByRole('link', { name: /favorite pokémon/i });
+    const navigation = [linkHome, linkAbout, linkFavorite];
+    navigation.forEach((link) => {
+      expect(link).toBeInTheDocument();
+    });
+  });
+});
