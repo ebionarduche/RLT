36849f41260751fcbb66fb9013ea65ab73529ca2
Ebio Narduche <ebionarduche77@gmail.com>
2023-05-03 09:41:55 -0300

    progresso requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste se a página contém um heading h2 com o texto Encountered Pokémon', () => {
+  it('Teste se a página contém um heading h2 com o texto Encountered Pokémon', () => {
+    renderWithRouter(<App />);
+    const encounteredPokemon = screen.getByRole('heading', { name: /encountered pokémon/i });
+    expect(encounteredPokemon).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', () => {
+    renderWithRouter(<App />);
+    const pikachu = screen.queryByRole('img', { name: /pikachu sprite/i });
+    expect(pikachu).toBeInTheDocument();
+    const buttonNext = screen.getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(buttonNext);
+    const charmander = screen.queryByRole('img', { name: /charmander sprite/i });
+    expect(charmander).toBeInTheDocument();
+  });
+  it('Teste se a Pokédex tem os botões de filtro:', () => {
+    renderWithRouter(<App />);
+    const typebtn = screen.getAllByTestId('pokemon-type-button');
+    const pokemonTypes = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+    typebtn.forEach((btn, index) => {
+      expect(btn).toHaveTextContent(pokemonTypes[index]);
+    });
+  });
+  it('', () => {
+
+  });
+});
